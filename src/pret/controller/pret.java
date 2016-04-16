675f1f10bde8eac0d8eccb2a58e15dbb5e8c0dbe
forgeron36 <alexandre.callet@outlook.com>
2016-04-16 19:57:48 +0200

    Removing several setters

--- src/pret/controller/pret.java
+++ src/pret/controller/pret.java
@@ -18,7 +18,8 @@ public class pret {
 		this.setStart_date(start_date);
 		this.setInterest_rate(interest_rate);
 		this.setLoan(loan);
-		this.amortization = new amortization_table(loan, interest_rate, duration_in_months);
+		this.setAmortization(new amortization_table(loan, interest_rate, duration_in_months));
+		
 		
 	}
 	//methods
@@ -43,31 +44,31 @@ public class pret {
 		return duration_in_months;
 	}
 
-	public void setDuration_in_months(int duration_in_months) {
+	private void setDuration_in_months(int duration_in_months) {
 		this.duration_in_months = duration_in_months;
 	}
 	public Date getStart_date() {
 		return start_date;
 	}
-	public void setStart_date(Date start_date) {
+	private void setStart_date(Date start_date) {
 		this.start_date = start_date;
 	}
 	public float getInterest_rate() {
 		return interest_rate;
 	}
-	public void setInterest_rate(float interest_rate) {
+	private void setInterest_rate(float interest_rate) {
 		this.interest_rate = interest_rate;
 	}
 	public double getLoan() {
 		return loan;
 	}
-	public void setLoan(double loan) {
+	private void setLoan(double loan) {
 		this.loan = loan;
 	}
 	public amortization_table getAmortization() {
 		return amortization;
 	}
-	public void setAmortization(amortization_table amortization) {
+	private void setAmortization(amortization_table amortization) {
 		this.amortization = amortization;
 	}
 	
